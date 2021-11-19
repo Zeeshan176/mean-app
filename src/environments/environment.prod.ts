79399460d8825201b40b2186b0073f6cc52efc4f
zeeshan176 <zeeshanmd956@gmail.com>
2021-11-19 16:19:16 +0530

    testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://nodeangular-env.kjipxmqrju.us-east-2.elasticbeanstalk.com/api"
+  apiUrl: "http://meanstackfinished-env.eba-iggbmrsy.ap-south-1.elasticbeanstalk.com/api"
 };
